9e2e6b252e016d262d30dd52f9dce51211425689
root <you@example.com>
2021-04-23 18:19:25 +0545

    commited

--- resources/views/mail/layout/master.blade.php
+++ resources/views/mail/layout/master.blade.php
@@ -1,115 +1,26 @@
-<!doctype html>
-<html lang="en">
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-
-  <!-- Bootstrap CSS -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-
-  <title>Email Template</title>
-</head>
-<body>
-  <style type="text/css">
-    .header_ground {
-    background-color: #539be2;
-    height: 233px;
-    text-align: center;
-}
-body{
-  background-color: #f4f4f4;
-}
-.body_ground {
-    width:47%;
-    background-color: white;
-    margin: -93px auto 0;
-    padding: 30px;
-}
-.body_ground_1{
-   width:47%;
-    margin: 0 auto;
-    padding: 30px;
-}
-.body_ground h1 {
-    font-size: 48px;
-    font-weight: 400;
-    text-transform: capitalize;
-    text-align: center;
-    margin-bottom: 50px;
-
-}
-.top_1 p {
-    font-size: 18px;
-    color: #777;
-}
-a.bth-primary {
-    background-color: #539be2;
-    color: #fff;
-    text-decoration: none;
-    padding: 10px 15px;
-    display: inline-block;
-}
-.bth_group {
-    text-align: center;
-    margin-top: 55px;
-}
-.top_2 p {
-    font-size: 18px;
-    color: #777;
-}
-.top_2 {
-    margin-top: 55px;
-}
-.team_msg p {
-    color: #777;
-        font-size: 18px;
-
-}
-
-.team_msg {
-    margin-top: 36px;
-}
-
-.hyper_link a {
-    color: #000;
-    font-size: 14px;
-}
-.small_text a {
-    color: #000;
-}
-
-.small_text {
-    font-size: 14px;
-    margin-top: 25px;
-    margin-bottom: 25px;
-        color: #777;
-}
-.contoso p {
-    font-size: 14px;
-      color: #777;
-}
-.branding_img {
-    height: 74px;
-    margin-top: 33px;
-}
-  </style>
-  <div class="header_ground">
-    <img class="branding_img" src="http://lyceex.abgroup/images/logo.png" alt="">
-  </div>
+<html>
+<body style="margin:0;padding:0;">
+  <div style="  
+    border: 15px solid #f0f5fc;width: 100%;color:rgb(61 65 101);font-family: calibri;    box-sizing: border-box;">
+  <div style="width: 100%;border-bottom:1px dashed #ddd;   box-sizing: border-box;">
+    
   @yield('mail-content')
  
-  </div> 
-  <div class="body_ground_1">
-    <div class="hyper_link">
-        <a href="">Dashboard</a> - <a href="">Billing</a> - <a href="">Help</a>
+  <div style="border-top:1px dotted #ddd;padding-top: 10px;">
+        Thanks, <br>
+        {{ config('app.name') }}
+        </div>
       </div>
+    </div>
+  </div>
 
-      <div class="small_text">
-        if these emails get annoying. please feel free to <a href="">Unsubscribe</a>
-      </div>
-      <div class="contoso"><p>contoso-1234 Main Street - Anywhere, MA - 56789</p></div>
-  </div> 
-
+  <div style="display: flex;align-items: center; text-align: center; margin:0;  box-sizing: border-box;width:100%;background:#5d83cc;padding:10px;color:#fff">
+    <div style="margin: 0 auto;display: flex;">
+    <img src="http://lyceex.com/images/logo.png" style="    width: 50px;margin-right:10px; "/>
+    <div><div style="font-size: 20px;">Lyceex</div>
+    <p style="margin: 5px 0;">@Copyright - Lyceex</p></div>
+  </div>
+  </div>
+</div>
 </body>
 </html>
--- resources/views/mail/verify-school.blade.php
+++ resources/views/mail/verify-school.blade.php
@@ -1,13 +1,16 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Welcome Email</title>
-  </head>
-  <body>
-    <h2>Welcome to the site {{$school['name']}}</h2>
-    <br/>
+
+@extends('mail.layout.master')
+
+@section('mail-content')
+    <h1 style=" margin: 0;   box-sizing: border-box;width:100%;background:#5d83cc;padding:20px;color:#fff;text-align:left">Welcome {{$school['name']}}</h1>
+    <div style="padding-left: 10px;padding-right: 10px;">
+      <div style="    display: inline-block;      padding: 10px;">
+        <br/>
         Your registered email-id is {{$school['email']}} , Please click on the below link to verify your email account
     <br/>
     <a href="{{url('/school/verify', $school->verifySchool->token)}}">Verify Email</a>
-  </body>
-</html>
+@endsection
+       
+
+    
+
